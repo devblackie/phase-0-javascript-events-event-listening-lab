a69fe321c827225a7cc014a998fa686ec1b77f35
devblackie <newtieblackie@gmail.com>
2022-06-05 19:35:21 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+    const input =document.getElementById('input');
+    input.addEventListener('click',function(){
+        alert('You are tearing me apart,Lisa!');
+    });
 }
