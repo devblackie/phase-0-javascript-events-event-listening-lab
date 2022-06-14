e72bb614fa4ea55f37de194572d481c1c05fd4d6
devblackie <newtieblackie@gmail.com>
2022-06-15 00:49:42 +0300

    commit tree d0ee27771bf75c9079fe683c7b23b2227e261e5e

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-function addingEventListener() {
-    const input =document.getElementById('input');
-    input.addEventListener('click',function(){
-        alert('You are tearing me apart,Lisa!');
-    });
+function clickAlert(){
+    alert('I was clicked!');
+}
+function addingEventListener(){
+    const input = document.getElementById('input');
+    input.addEventListener('click',clickAlert);
 }
